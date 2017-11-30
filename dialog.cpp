f57a2504dfc980be4fc81eb8686f50d800c6b0ad
Rodrigo I Gerbasi <rgerbasi@mc18.cs.purdue.edu>
2017-11-30 18:51:51 -0500

    Make IRCclient

--- dialog.cpp
+++ dialog.cpp
@@ -194,7 +194,7 @@ void Dialog::newUserAction()
 {
     printf("New User Button\n");
     char response[MAX_RESPONSE];
-    client->sendCommand(client->host, client->port, "ADD-USER Bard Bard",(char *) response);
+    client->sendCommand(client->host, client->port, "ADD-USER Bard Bard", response);
 
 
 }
