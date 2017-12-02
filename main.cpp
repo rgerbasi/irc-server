16f0d15b73c147859ce8bb699a0fce1ba2a206c1
Rodrigo I Gerbasi <rgerbasi@moore23.cs.purdue.edu>
2017-12-02 16:49:10 -0500

    Make IRCclient

--- main.cpp
+++ main.cpp
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     //IRCClient * client = new IRCClient(argc, argv);
-    Dialog dialog(argc, argv);
+    Verification dialog(argc, argv);
     dialog.show();
 
     return app.exec();
