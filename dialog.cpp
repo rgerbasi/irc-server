ed74a5ded46022ba513fb834b1c7df2094fe5936
Rodrigo I Gerbasi <rgerbasi@mc18.cs.purdue.edu>
2017-12-03 16:11:06 -0500

    Make IRCclient

--- dialog.cpp
+++ dialog.cpp
@@ -68,6 +68,7 @@
 #include <stdlib.h>
 
 #define MAX_RESPONSE (10 * 1024)
+//IRCClient
 IRCClient::IRCClient(){
     //constructor
     this->host = "";
@@ -79,10 +80,80 @@ IRCClient::IRCClient(){
 
 int IRCClient::open_client_socket(char * host, int port){
     //open client socket
+    // Initialize socket address structure
+    struct  sockaddr_in socketAddress;
 
+    // Clear sockaddr structure
+    memset((char *)&socketAddress,0,sizeof(socketAddress));
+
+    // Set family to Internet
+    socketAddress.sin_family = AF_INET;
+
+    // Set port
+    socketAddress.sin_port = htons((u_short)port);
+
+    // Get host table entry for this host
+    struct  hostent  *ptrh = gethostbyname(host);
+    if ( ptrh == NULL ) {
+        perror("gethostbyname");
+        exit(1);
+    }
+
+    // Copy the host ip address to socket address structure
+    memcpy(&socketAddress.sin_addr, ptrh->h_addr, ptrh->h_length);
+
+    // Get TCP transport protocol entry
+    struct  protoent *ptrp = getprotobyname("tcp");
+    if ( ptrp == NULL ) {
+        perror("getprotobyname");
+        exit(1);
+    }
+
+    // Create a tcp socket
+    int sock = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+    if (sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    // Connect the socket to the specified server
+    if (connect(sock, (struct sockaddr *)&socketAddress,
+            sizeof(socketAddress)) < 0) {
+        perror("connect");
+        exit(1);
+    }
+    return sock;
 }
 int IRCClient::sendCommand(char *  host, int port, char * command, char * response){
     //send command
+
+    int sock = open_client_socket( host, port);
+
+    if (sock<0) {
+        return 0;
+    }
+
+    // Send command
+    write(sock, command, strlen(command));
+    write(sock, "\r\n",2);
+
+    //Print copy to stdout
+    write(1, command, strlen(command));
+    write(1, "\r\n",2);
+
+    // Keep reading until connection is closed or MAX_REPONSE
+    int n = 0;
+    int len = 0;
+    while ((n=read(sock, response+len, MAX_RESPONSE - len))>0) {
+        len += n;
+    }
+    response[len]=0;
+
+    printf("response:\n%s\n", response);
+
+    close(sock);
+
+    return 1;
 }
 //end of IRC CLIENT class
 //slots of verification class
